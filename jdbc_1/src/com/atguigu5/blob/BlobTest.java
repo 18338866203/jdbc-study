33bb4837de7296699333ebd0b9b05ffa4e825c39
wangyj <486943041@qq.com>
2020-10-06 15:58:55 +0800

    Blob数据处理以及批量插入操作

---
+++ jdbc_1/src/com/atguigu5/blob/BlobTest.java
@@ -0,0 +1,96 @@
+package com.atguigu5.blob;
+
+import com.atguigu3.bean.Customer;
+import com.atguigu3.util.JDBCUtils;
+import org.junit.Test;
+
+import java.io.*;
+import java.sql.*;
+
+
+/**
+ * @author wyj
+ * @Description 测试使用PreparedStatement操作Blob类型的数据
+ * @create 2020-10-06 15:18
+ */
+public class BlobTest {
+    //向数据表customers中插入Blob类型的字段
+    @Test
+    public void testInsert(){
+        Connection conn = null;
+        PreparedStatement ps = null;
+        try {
+            conn = JDBCUtils.getConnection();
+            String sql = "insert into customers(name,email,birth,photo) values(?,?,?,?)";
+            ps = conn.prepareStatement(sql);
+            ps.setObject(1,"袁浩");
+            ps.setObject(2,"yuan@qq.com");
+            ps.setObject(3,"1992-09-08");
+            FileInputStream fis = new FileInputStream(new File("playgirl.jpg"));
+            ps.setBlob(4,fis);
+            ps.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            JDBCUtils.closeResource(conn,ps);
+        }
+    }
+
+    //查询数据表customers中Blob类型的字段
+    @Test
+    public void testQuery(){
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        InputStream is = null;
+        FileOutputStream fos = null;
+        try {
+            conn = JDBCUtils.getConnection();
+            String sql = "select id,name,email,birth,photo from customers where id = ?";
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1,19);
+            rs = ps.executeQuery();
+            if (rs.next()){
+                //			方式一：
+                //			int id = rs.getInt(1);
+                //			String name = rs.getString(2);
+                //			String email = rs.getString(3);
+                //			Date birth = rs.getDate(4);
+                //方式二：
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                Date birth = rs.getDate("birth");
+                Customer cust = new Customer(id, name, email, birth);
+                System.out.println(cust);
+                //将Blob类型的字段下载下来，以文件的方式保存在本地
+                Blob photo = rs.getBlob("photo");
+                is = photo.getBinaryStream();
+                fos = new FileOutputStream("yuan.jpg");
+                byte[] buffer = new byte[1024];
+                int len;
+                while ((len = is.read(buffer)) != -1){
+                    fos.write(buffer,0,len);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            if(is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            JDBCUtils.closeResource(conn,ps,rs);
+        }
+    }
+}
